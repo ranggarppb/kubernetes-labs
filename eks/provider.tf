e4df2b4b000073b941dbdc7188f80e4dd1fdeeac
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-27 18:38:41 +0700

    feat: add eks provisioning

---
+++ eks/provider.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+  shared_credentials_files = ["~/.aws/credentials"]
+  shared_config_files      = ["~/.aws/config"]
+  profile                  = "ranggarppb"
+
+  region = "ap-southeast-1"
+}
+
+terraform {
+  backend "s3" {
+    profile = "ranggarppb"
+    key     = "eks/terraform.tfstate"
+    bucket  = "kubernetes-labs"
+    region  = "ap-southeast-1"
+    encrypt = true
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
