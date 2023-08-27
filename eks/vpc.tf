ebfbbd710d07b5f621f308b13e3be1f0d7d48fa2
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-27 19:15:24 +0700

    feat: initialize github workflow and eks (#1)
    * feat: add github action oidc provisioning
    
    * feat: add eks provisioning
    
    * chore: add branch clone repository step
    
    * fix: trigger
    
    * fix: trigger
    
    * fix: try to run from default branch

---
+++ eks/vpc.tf
@@ -0,0 +1,12 @@
+resource "aws_vpc" "kubernetes_labs" {
+  cidr_block = "192.168.0.0/16"
+
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  assign_generated_ipv6_cidr_block = false
+
+  tags = {
+    Name = "main vpc"
+  }
+}
