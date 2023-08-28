ccedde212e11f28ea3cd954586e8acfd1f00cec5
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-28 10:09:31 +0700

    fix: change route to object

--- eks/routing-tables.tf
+++ eks/routing-tables.tf
@@ -1,12 +1,11 @@
 resource "aws_route_table" "kubernetes_labs_public" {
   vpc_id = aws_vpc.kubernetes_labs.id
 
-  route = [
-    {
-      cidr_block = "0.0.0.0/0"
-      gateway_id = aws_internet_gateway.kubernetes_labs.id
-    }
-  ]
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.kubernetes_labs.id
+  }
+
   tags = {
     Name = "main-public"
   }
@@ -15,12 +14,11 @@ resource "aws_route_table" "kubernetes_labs_public" {
 resource "aws_route_table" "kubernetes_labs_private_1" {
   vpc_id = aws_vpc.kubernetes_labs.id
 
-  route = [
-    {
-      cidr_block     = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.kubernetes_labs_nat1
-    }
-  ]
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.kubernetes_labs_nat1
+  }
+
   tags = {
     Name = "main-private-1"
   }
@@ -29,12 +27,11 @@ resource "aws_route_table" "kubernetes_labs_private_1" {
 resource "aws_route_table" "kubernetes_labs_private_2" {
   vpc_id = aws_vpc.kubernetes_labs.id
 
-  route = [
-    {
-      cidr_block     = "0.0.0.0/0"
-      nat_gateway_id = aws_nat_gateway.kubernetes_labs_nat2
-    }
-  ]
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.kubernetes_labs_nat2
+  }
+
   tags = {
     Name = "main-private-2"
   }
