ed3fa99e64ca4824a1d9a06775bf75fe20508569
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-31 04:46:07 +0700

    refactor: use terraform module (#10)
    * feat: use module
    
    * feat: use module
    
    * feat: use module
    
    * fix: add vars
    
    * fix: add vars
    
    * fix: add vars
    
    * fix: add vars
    
    * fix: move ca certificate script and terraform version to root
    
    * feat: use terraform registry
    
    * feat: use terraform registry

---
+++ eks/main.tf
@@ -0,0 +1,9 @@
+module "terraform_eks_labs" {
+  source = "ranggarppb/eks-labs/aws"
+  version = "0.1.0"
+
+  region             = var.region
+  availability_zones = var.availability_zones
+  aws_user_id        = var.aws_user_id
+  cluster_name       = var.cluster_name
+}
--- eks/variables.tf
+++ eks/variables.tf
@@ -1,3 +1,26 @@
+variable "region" {
+  type        = string
+  description = "Region to execute the AWS provider and create VPC"
+  nullable    = false
+}
+
+variable "availability_zones" {
+  type        = list(string)
+  description = "Availability zones to provision subnets"
+  nullable    = false
+
+  validation {
+    condition     = length(var.availability_zones) == 2
+    error_message = "Specify exactly two availability zones in the region"
+  }
+}
+
+variable "aws_user_id" {
+  type        = string
+  description = "AWS User ID"
+  nullable    = false
+}
+
 variable "cluster_name" {
   type        = string
   description = "Cluster name"
