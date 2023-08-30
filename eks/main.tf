7eabec20b96282df1dc43213a9324dbba2d21428
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-31 01:05:40 +0700

    feat: use module

--- eks/main.tf
+++ eks/main.tf
@@ -1,5 +1,5 @@
 module "terraform_eks_labs" {
-  source = "https://github.com/ranggarppb/terraform-eks-labs.git"
+  source = "github.com/ranggarppb/terraform-eks-labs.git"
 
   region             = "ap-southeast-1"
   availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
