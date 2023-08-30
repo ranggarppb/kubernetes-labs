e21a97b1bab624d3a672edbc53455279cd2a1e6e
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-30 16:37:35 +0700

    fix: try using bash script

--- eks/cluster_ca_certificate.sh
+++ eks/cluster_ca_certificate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cluster_ca_certificate=$(aws eks describe-cluster --name $1 --query cluster.certificateAuthority.data)
-echo -n "{\"data\":\"${cluster_ca_certificate}\"}"
+echo "{\"data\":${cluster_ca_certificate}}"
