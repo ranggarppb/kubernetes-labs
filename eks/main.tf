8b036ce03ad06214c639b0f248dffcb855cb47f0
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-31 01:03:26 +0700

    feat: use module

---
+++ eks/main.tf
@@ -0,0 +1,7 @@
+module "terraform_eks_labs" {
+  source = "https://github.com/ranggarppb/terraform-eks-labs.git"
+
+  region             = "ap-southeast-1"
+  availability_zones = ["ap-southeast-1a", "ap-southeast-1b"]
+  aws_user_id        = "020612890700"
+}
