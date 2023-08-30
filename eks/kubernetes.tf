5f78ef0d03e2a11425733555d70f493a4a2d59f1
Rangga Putra Pertama <ranggaputrapertamapp@gmail.com>
2023-08-30 19:21:36 +0700

    feat: provision configmap (#8)
    * feat: add provisioning for config map
    
    * feat: add kubernetes cluster auth
    
    * feat: add kubernetes cluster auth
    
    * fix: set tf log to debug
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: query certificate in argument
    
    * fix: try using bash script
    
    * fix: try using bash script
    
    * fix: add var cluster name in github action
    
    * fix: add var cluster name in github action
    
    * fix: change map roles and users to list
    
    * fix: change map roles and users to list
    
    * fix: change map roles and users to list
    
    * fix: change map roles and users to list

---
+++ eks/kubernetes.tf
@@ -0,0 +1,38 @@
+locals {
+  map_roles = [
+    {
+      rolearn  = "arn:aws:iam::020612890700:role/eks-workers"
+      username = "system:node:{{EC2PrivateDNSName}}"
+      groups = [
+        "system:bootstrappers",
+        "system:nodes"
+      ]
+    }
+  ]
+  map_users = [
+    {
+      userarn  = "arn:aws:iam::020612890700:root"
+      username = "ranggarppb"
+      groups = [
+        "system:masters"
+      ]
+    }
+  ]
+}
+
+resource "kubernetes_config_map" "kubernetes_labs" {
+  metadata {
+    name      = "aws-auth"
+    namespace = "kube-system"
+    labels    = {}
+  }
+
+  count = 1
+
+  data = {
+    mapRoles = yamlencode(local.map_roles)
+    mapUsers = yamlencode(local.map_users)
+  }
+
+  depends_on = [aws_eks_cluster.kubernetes_labs]
+}
